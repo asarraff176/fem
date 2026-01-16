e7c95c8f3ce1cfedb797efc17c6ac1fdc3b2854a
codex.an14 <global.an14@mac>
2026-01-16 00:33:54 -0400

    Sala app module - It can just calculate the stiffness of a Bar in compression

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import numpy as np
 
 from core.two_dimension_space import get_truss_local_stiffness_matrix
   
@@ -23,14 +23,14 @@ def add(a: int, b: int) -> dict[str, int]:
 
 
 @app.post("/get_truss_local_stiffness_matrix")
-def get_truss_local_stiffness_matrix_endpoint(data: dict[str, float]) -> dict[str, list[float]]: 
+def get_truss_local_stiffness_matrix_endpoint(data: dict[str, float]) -> dict[str, float]: 
     
     area: float = data["area"]
     elastic_modulus: float = data["elastic_modulus"]
     length: float = data["length"]
 
-    k_matrix = get_truss_local_stiffness_matrix(area, elastic_modulus, length)
-    k = k_matrix[0].to_list()
+    k_matrix: np.ndarray[tuple[Any, ...], np.dtype[np.Any]] = get_truss_local_stiffness_matrix(area, elastic_modulus, length)
+    k: list[float] = round(float(k_matrix[0][0]),3)  # Just return the first row for simplicity
 
     return {
         "stiffness": k
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,26 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-
-
-
 function App() {
   const [area, setArea] = useState(10);
   const [youngModulus, setYoungModulus] = useState(100);
   const [length, setLength] = useState(1000);
   const [k, setK] = useState([1, 2]);
 
-  async function calculate() {
-    const res = await fetch("http://localhost:8000/add?a=2&b=3")
-    const data = await res.json()
-
-    setK(data.result)
-  }
-
   async function calculate_2() {
 
-    const data = { "area": 2.0, "elastic_modulus": 3.0, "length": 4.0};
+    const data = { "area": area, "elastic_modulus": youngModulus, "length": length };
 
     const res = await fetch("http://localhost:8000/get_truss_local_stiffness_matrix", {
       method: 'POST',
@@ -39,11 +28,34 @@ function App() {
 
   return (
     <>
-      <input type='number' placeholder='Area' value={area} onChange={e => setArea(Number(e.target.value))} />
-      <input type='number' placeholder='Young Modulus' value={youngModulus} onChange={e => setYoungModulus(Number(e.target.value))} />
-      <input type='number' placeholder='Length' value={length} onChange={e => setLength(Number(e.target.value))} />
-      <button onClick={calculate_2}>Submit!</button>
-      <p>Stiffness: {k}</p>
+    <h1>Structural Sala App</h1>
+    <div class="main-app">
+    <div class="sidebar">
+      <p>App 1</p>
+      <p>App 2</p>
+      <p>App 3</p>
+      <p>App 4</p>
+      <p>App 5</p>
+    </div>
+    <div class="current-page">
+      <table>
+        <thead>
+          <tr><td>id</td> <td> Area </td> <td> Young Modulus </td> <td> Length </td> <td> Stiffness </td></tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td><input type='number' placeholder='Area' value={area} onChange={e => setArea(Number(e.target.value))} /> </td>
+            <td> <input type='number' placeholder='Young Modulus' value={youngModulus} onChange={e => setYoungModulus(Number(e.target.value))} /> </td>
+            <td><input type='number' placeholder='Length' value={length} onChange={e => setLength(Number(e.target.value))} />
+             </td>
+             <td> {k} </td>
+             <td>  <button onClick={calculate_2}>Submit!</button> </td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+</div>
     </>
   )
 }
